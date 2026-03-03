6458e9bb91516fc098635fa0fb5eaf605fedbc11
mndrake95 <mandra4a@gmail.com>
2026-03-03 15:42:11 +0500

    test: first red tests for getting amount by category and month

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,12 +1,11 @@
 import pytest
 from pathlib import Path
-from services.transaction_service import filter_by_month, filter_by_category
+from services.transaction_service import filter_by_month, filter_by_category, calculate_total, get_totals_by_category, get_totals_by_month
 
 transactions = [
     {"date": "2024-01-15", "description": "ONAY.KZ", "category": "Покупка", "amount": -1000.0},
     {"date": "2024-01-20", "description": "Анна К.", "category": "Пополнение", "amount": 5000.0},
     {"date": "2024-02-05", "description": "YANDEX.GO", "category": "Покупка", "amount": -500.0},
-
 ]
 
 def test_filter_by_month():
@@ -16,3 +15,21 @@ def test_filter_by_month():
 def test_filter_by_category():
     filter = filter_by_category(transactions, "Покупка")
     assert len(filter) == 2
+
+def test_calculate_total():
+    result = calculate_total(transactions)
+    assert result == sum(t["amount"] for t in transactions)
+
+def test_calculate_total_empty():
+    result = calculate_total([])
+    assert result == 0.0
+
+def test_get_totals_by_category():
+    result = get_totals_by_category(transactions)
+    sum_result = sum(result.values())
+    assert sum_result == sum(t["amount"] for t in transactions)
+
+def test_get_totals_by_month():
+    result = get_totals_by_month(transactions)
+    sum_result = sum(result.values())
+    assert sum_result == sum(t["amount"] for t in transactions)
